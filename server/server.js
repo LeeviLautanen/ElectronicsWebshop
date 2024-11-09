b339e9782d7a63fe4b6fb12235bb4e2a7ada93d4
Leevi Lautanen <leevi.lautanen02@gmail.com>
2024-11-09 02:41:27 +0200

    Added server dockerfile

--- server/server.js
+++ server/server.js
@@ -9,15 +9,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Connect to MongoDB
-mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("Database connected"))
-  .catch((err) => console.log("Database connection error: ", err));
-
 // Test route
 app.get("/", (req, res) => {
   res.send("Webshop backend");
