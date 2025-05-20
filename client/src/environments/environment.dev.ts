06049e14db1f1b7c427f4ec3ea89ff65b80091c0
Leevi Lautanen <leevi.lautanen02@gmail.com>
2025-05-20 21:08:29 +0300

    Corrected old 1234 port to 443

--- client/src/environments/environment.dev.ts
+++ client/src/environments/environment.dev.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  baseUrl: `https://${window.location.hostname}:1234`,
+  baseUrl: `https://${window.location.hostname}`,
   clientId:
     'Ad-EWT93KQ6N3yBFFUfefPTu1k3P6DeJ56oB0tIaN4Ld1t9_39okirUNhJPU_r4i3m7rLhLDeF4zkEJ_',
 };
