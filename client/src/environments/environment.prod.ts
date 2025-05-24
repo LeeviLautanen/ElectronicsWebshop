212e6b3fde2885ccc470fb810243f1aa3ff2a835
Leevi Lautanen <leevi.lautanen02@gmail.com>
2025-05-24 14:55:22 +0300

    Added a test environment to client

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,6 +1,4 @@
 export const environment = {
   production: true,
   baseUrl: 'https://bittiboksi.fi',
-  clientId:
-    'ASJHFDhPccUshMsQQzgDx0iMh3Vntu9UVBI3Hu8SjZJgLmHBgAajbkwtv_iZ6BcyUELtFnDwDu7siGDM',
 };
